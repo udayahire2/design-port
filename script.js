86902933003ba401d61f920b2a09ae6017d803de
udayahire2 <surekhaahire855@gmail.com>
2026-03-26 16:52:16 +0530

    created about section

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const container = document.querySelector(".background");
 // duplicate once (mandatory)
 container.innerHTML += container.innerHTML;
 
+const btn = document.getElementById("menuBtn");
+const nav = document.getElementById("dropdownNav");
 // calculate width AFTER duplication
 const totalWidth = container.scrollWidth / 2;
 
@@ -11,4 +13,8 @@ gsap.to(container, {
   duration: 80,
   ease: "none",
   repeat: -1
+});
+
+btn.addEventListener("click", () => {
+    nav.classList.toggle("hidden");
 });
